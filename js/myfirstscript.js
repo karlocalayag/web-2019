3cd8771ab466246c82e4ba92fce4b4540ab90aed
Karlo <3151240@student.ocadu.ca>
2019-10-09 14:52:58 -0400

    Karlo updated his files

---
+++ js/myfirstscript.js
@@ -0,0 +1,9 @@
+//alert("my first event");
+
+//console.log("this is my secret message to me");
+
+$("button").click(function(){
+	alert("my first REAL function")
+})
+
+$("body").css("background", "red")
